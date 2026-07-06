c1b573f9d6ec665c16155a0dc8316ba24a6c4afc
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-07-07 02:28:02 +0600

    added the auth schema model

---
+++ server/src/app/modules/auth/auth.model.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const { USER_ROLE } = require("./auth.constant");
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+        },
+
+        password: {
+            type: String,
+            required: true,
+            select: 0,
+        },
+
+        role: {
+            type: String,
+            enum: Object.values(USER_ROLE),
+            required: true,
+        },
+
+        isActive: {
+            type: Boolean,
+            default: true,
+        },
+
+        lastLogin: Date,
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = mongoose.model("User", userSchema);
