2a9afe09c5898d8299b0c9949924cdc3dfd0005e
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-07-07 03:58:46 +0600

    added the appoinment constant

---
+++ server/src/app/modules/appointment/appointment.constant.js
@@ -0,0 +1,10 @@
+const APPOINTMENT_STATUS = {
+    WAITING: "waiting",
+    SERVING: "serving",
+    COMPLETED: "completed",
+    CANCELLED: "cancelled",
+};
+
+module.exports = {
+    APPOINTMENT_STATUS,
+};
