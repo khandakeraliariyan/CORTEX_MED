ba9748fa692e5a50bf2ba1b98ae01f43ed1dd5cd
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-07-07 04:06:19 +0600

    added the appoinment routes and connected the router

---
+++ server/src/app/modules/appointment/appointment.route.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const router = express.Router();
+
+const AppointmentController = require("./appointment.controller");
+
+const validateRequest = require("../../middlewares/validateRequest");
+
+const auth = require("../../middlewares/auth");
+
+const { createAppointmentValidation } = require("./appointment.validation");
+
+router.post("/", auth("admin", "receptionist"), validateRequest(createAppointmentValidation), AppointmentController.createAppointment);
+
+module.exports = router;
--- server/src/app/routes/index.js
+++ server/src/app/routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 
 const authRoutes = require("../modules/auth/auth.route");
 const doctorRoutes = require("../modules/doctor/doctor.route");
+const appointmentRoutes = require("../modules/appointment/appointment.route");
 
 router.use("/auth", authRoutes);
 router.use("/doctors", doctorRoutes);
+router.use("/appointments", appointmentRoutes);
 
 router.get("/", (req, res) => {
     res.json({
