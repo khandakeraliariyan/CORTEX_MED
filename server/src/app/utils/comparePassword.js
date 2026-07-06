bcdf23f1f6caf3c1c2bbdbd8d71d24fe601287c7
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-07-07 02:30:15 +0600

    added the compare pass utility function

---
+++ server/src/app/utils/comparePassword.js
@@ -0,0 +1,13 @@
+const bcrypt = require("bcrypt");
+
+const comparePassword = async (
+    plainPassword,
+    hashedPassword
+) => {
+    return await bcrypt.compare(
+        plainPassword,
+        hashedPassword
+    );
+};
+
+module.exports = comparePassword;
