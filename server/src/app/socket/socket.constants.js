556898e3cebf47c755471e72b855c63f313fb8fb
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-07-07 17:15:13 +0600

    added the socket constants

---
+++ server/src/app/socket/socket.constants.js
@@ -0,0 +1,16 @@
+const SOCKET_EVENTS = {
+    CONNECTION: "connection",
+    DISCONNECT: "disconnect",
+
+    QUEUE_UPDATED: "queue:updated",
+
+    PATIENT_CALLED: "patient:called",
+
+    PATIENT_COMPLETED: "patient:completed",
+
+    WAIT_UPDATED: "wait:updated",
+};
+
+module.exports = {
+    SOCKET_EVENTS,
+};
