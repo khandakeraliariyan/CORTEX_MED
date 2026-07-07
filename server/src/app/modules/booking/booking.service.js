674b6c82ea6a855a45dddf0e813a41548de5b85e
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-07-07 14:07:42 +0600

    updated the booking services

--- server/src/app/modules/booking/booking.service.js
+++ server/src/app/modules/booking/booking.service.js
@@ -1,20 +1,19 @@
 const AppointmentService = require("../appointment/appointment.service");
 const TriageService = require("../triage/triage.service");
-const QueueService = require("../queue/queue.service");
 const WaitTimeService = require("../wait-time/waitTime.service");
 
 const bookPatient = async (payload) => {
     const appointment = await AppointmentService.createAppointment(payload);
 
-    const triage = await TriageService.runTriage(appointment._id);
+    const updatedAppointment = await TriageService.runTriage(
+        appointment._id
+    );
 
-    await WaitTimeService.recalculateQueue(appointment.doctor);
+    await WaitTimeService.recalculateQueue(
+        updatedAppointment.doctor
+    );
 
-    return {
-        appointment,
-        triage,
-        queue,
-    };
+    return updatedAppointment;
 };
 
 module.exports = {
